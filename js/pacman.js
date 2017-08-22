bb232dfe263ff1c4dc57ec0ae60bab560f27982d
anzarsh <anzarsh@mail.ru>
2017-08-22 13:21:01 +0300

    round walk and first map drawing

--- js/pacman.js
+++ js/pacman.js
@@ -1,76 +1,94 @@
 (function(){
-	var Pacman = function(ctx, radius, x, y){
-		this.ctx = ctx;
-		this.radius = radius;
-		this.x = x;
-		this.y = y;
-		this.state = 1;
+	var Pacman = function(canvas, size, radius, x, y){
+		this.canvas = canvas;
+		this.ctx = this.canvas.getContext("2d");
+		this.size = size;
+		this.radius = radius * this.size;
+		this.screenX = x;
+		this.screenY = y;
+		this.state = 20;
 		this.direction = 0;
-		this.step = 1;
-		this.paint();
+		this.step = 1 * this.size;
+		this.stateEnd = 38;
+	};
+
+	Pacman.prototype._paint = function(color, radiusPlus){
+		var xs = [];
+		var ys = [];
+		xs.push(this.screenX);
+		ys.push(this.screenY);
+		if (this.screenX < this.radius){
+			xs.push(this.screenX + this.canvas.width);
+		} else if (this.screenX > this.canvas.width - this.radius - 1){
+			xs.push(this.screenX - this.canvas.width);
+		}
+		if (this.screenY < this.radius){
+			ys.push(this.screenY + this.canvas.height);
+		} else if (this.screenY > this.canvas.height - this.radius - 1){
+			ys.push(this.screenY - this.canvas.height);
+		}
+		var stateEnd2 = this.stateEnd/2;
+		var stateEnd3 = 3*this.stateEnd/2;
+
+		var state = this.state > stateEnd2 ? this.stateEnd - this.state : this.state;
+		var topAngle = (this.direction * (Math.PI/2)) - (state*(Math.PI/stateEnd3));
+		var bottomAngle = (this.direction * (Math.PI/2)) + (state*(Math.PI/stateEnd3));
+		for(var i = 0; i < xs.length ; ++i){
+			for(var j = 0; j < ys.length ; ++j){
+				this.ctx.beginPath();
+				this.ctx.fillStyle = color;
+				this.ctx.arc(xs[i], ys[j], this.radius + radiusPlus, topAngle, topAngle + Math.PI, true);
+				this.ctx.fill();
+
+				this.ctx.beginPath();
+				this.ctx.fillStyle = color;
+				this.ctx.arc(xs[i], ys[j], this.radius + radiusPlus, bottomAngle, bottomAngle + Math.PI, false);
+				this.ctx.fill();
+			}
+		}
 	};
 
 	Pacman.prototype.paint = function(){
-		var state = this.state == 3 ? 1 : this.state;
-		var topAngle = (this.direction * (Math.PI/2)) - (state*(Math.PI/8));
-		var bottomAngle = (this.direction * (Math.PI/2)) + (state*(Math.PI/8));
-
-		this.ctx.beginPath();
-		this.ctx.fillStyle = "yellow";
-		this.ctx.arc(this.x, this.y, this.radius, topAngle, topAngle + Math.PI, true);
-		this.ctx.fill();
-
-		this.ctx.beginPath();
-		this.ctx.fillStyle = "yellow";
-		this.ctx.arc(this.x, this.y, this.radius, bottomAngle, bottomAngle + Math.PI, false);
-		this.ctx.fill();
+		this._paint("yellow", 0);
 	};
 
 	Pacman.prototype.clear = function(){
-		var state = this.state == 3 ? 1 : this.state;
-		var topAngle = (this.direction * (Math.PI/2)) - (state*(Math.PI/8));
-		var bottomAngle = (this.direction * (Math.PI/2)) + (state*(Math.PI/8));
-
-		this.ctx.beginPath();
-		this.ctx.fillStyle = "black";
-		this.ctx.arc(this.x, this.y, this.radius + 1, topAngle, topAngle + Math.PI, true);
-		this.ctx.fill();
-
-		this.ctx.beginPath();
-		this.ctx.fillStyle = "black";
-		this.ctx.arc(this.x, this.y, this.radius + 1, bottomAngle, bottomAngle + Math.PI, false);
-		this.ctx.fill();
+		this._paint("black", 1);
 	};
 
 	Pacman.prototype.left = function(){
 		this.clear();
-		this.x -= this.step;
+		this.screenX -= this.step;
+		this.screenX = (this.screenX<0)?this.canvas.width-1:this.screenX;
 		this.direction = 2;
-		this.state = (++this.state) % 4;
+		this.state = (++this.state) % this.stateEnd;
 		this.paint();
 	};
 
 	Pacman.prototype.right = function(){
 		this.clear();
-		this.x += this.step;
+		this.screenX += this.step;
+		this.screenX %= this.canvas.width;
 		this.direction = 0;
-		this.state = (++this.state) % 4;
+		this.state = (++this.state) % this.stateEnd;
 		this.paint();
 	};
 
 	Pacman.prototype.top = function(){
 		this.clear();
-		this.y -= this.step;
+		this.screenY -= this.step;
+		this.screenY = (this.screenY<0)?this.canvas.height-1:this.screenY;
 		this.direction = 3;
-		this.state = (++this.state) % 4;
+		this.state = (++this.state) % this.stateEnd;
 		this.paint();
 	};
 
 	Pacman.prototype.bottom = function(){
 		this.clear();
-		this.y += this.step;
+		this.screenY += this.step;
+		this.screenY %= this.canvas.height;
 		this.direction = 1;
-		this.state = (++this.state) % 4;
+		this.state = (++this.state) % this.stateEnd;
 		this.paint();
 	};
 
--- js/start.js
+++ js/start.js
@@ -1,14 +1,22 @@
 (function(){
-	var Start = function(ctx){
-		
+	var Start = function(){
 		var self = this;
 		
-		this.ctx = ctx;
-		this.pacman = new pacman.Pacman(ctx, 7, 100, 100);
+		this.size = 1;
+
+		this.canvas = document.createElement("canvas");		
+		this.ctx = this.canvas.getContext("2d");
+
+		this.pacman = new pacman.Pacman(self.canvas, this.size, 7, 100, 100);
 		
 		this.map = new pacman.Map();
 		this.map.load("map/lvl1.map", function(map){
-			console.log(map);
+			self.canvas.width = self.size * 8 * map.width;
+			self.canvas.height = self.size * 8 * map.height;
+			self.canvas.style.backgroundColor = "black";
+			document.body.appendChild(self.canvas);
+			self.drawMap(map);
+			self.pacman.paint();
 		}, function(err){
 			console.error(err);
 		});
@@ -30,24 +38,24 @@
 				if (key == 37){
 				
 					self.keys[key] = setInterval(function(){
-						game.pacman.left();
+						self.pacman.left();
 					}, self.stepDelay);
 				
 				} else	if (key == 38){
 				
 					self.keys[key] = setInterval(function(){
-						game.pacman.top();
+						self.pacman.top();
 					}, self.stepDelay);
 				
 				} else if (key == 39){
 				
 					self.keys[key] = setInterval(function(){
-						game.pacman.right();
+						self.pacman.right();
 					}, self.stepDelay);
 				
 				} else if (key == 40){
 					self.keys[key] = setInterval(function(){
-						game.pacman.bottom();
+						self.pacman.bottom();
 					}, self.stepDelay);
 				
 				}
@@ -63,6 +71,20 @@
 			}
 		});
 
+	};
+
+	Start.prototype.drawMap = function(map){
+		var height = map.height;
+		var width = map.width;
+		this.ctx.beginPath();
+		this.ctx.fillStyle = "blue";
+		for (var j = 0; j < height; j++){
+			for (var i = 0; i < width; i++){
+				if(map.data[j*width+i] === "1"){
+					this.ctx.fillRect(((i*8)+1), ((j*8)+1), 6, 6);
+				}
+			}
+		}
 	};
 
 	window.pacman = window.pacman || {};
